ab7c2d33271511be056cf2a28b039fc7e9fca4f4
mcmorriss <michaelmorriss14@gmail.com>
2022-03-17 18:16:47 -0500

    Added bio.js route to server.js for bio updates

---
+++ server/routes/bio.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import pool from '../helpers/database.js';
+
+const router = express.Router();
+
+
+const updateBio = router.put('/bio', async (req, res) => {
+	try{
+
+		const sqlQuery = 'UPDATE Profiles SET bio = ? WHERE username = ?';
+        const bio = req.body.bio;
+        const username = req.body.username;
+
+		const rows = await pool.query(sqlQuery, [bio, username]);
+		res.status(200).json(rows);
+
+	} catch(error){
+		res.status(400).send(error.message);
+
+	}
+
+});
+
+export { updateBio };
