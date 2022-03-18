f509992323e7fb4cc177f5dae1a57c2632ccc9be
Alec Moldovan <alec_moldovan@yahoo.com>
2022-03-17 23:40:59 -0500

    Deleted duplicate register form.

--- client/src/components/screens/SignUp.js
+++ client/src/components/screens/SignUp.js
@@ -10,14 +10,11 @@ const SignUp = () => {
 
   const [username, setUsername] = useState();
   const [media, setMedia] = useState();
-<<<<<<< HEAD
   const [error, setError] = useState();
   const [post, setPost] = useState();
-=======
   const [name, setName] = useState();
   const [password, setPassword] = useState();
 
->>>>>>> ab7c2d33271511be056cf2a28b039fc7e9fca4f4
   const navigate = useNavigate;
 
   const send = event =>{
@@ -99,67 +96,6 @@ const SignUp = () => {
 
       </Row>
 
-      <h1>Register</h1>
-      
-      <form action="#">
-        <div className="flex">
-          <label htmlFor="full_name">Name</label>
-          <input 
-            required 
-            type="text" 
-            id="full_name"
-            name="full_name"
-            placeholder="Enter name"
-            onChange={event => {
-              const { value } = event.target;
-              setName(value);
-          }}
-          />
-        </div>
-        <div className="flex">
-          <label htmlFor="username">Username</label>
-          <input 
-            required
-            type="text" 
-            id="username"
-            name="username"
-            placeholder="Enter Username"
-            onChange={event => {
-              const { value } = event.target;
-              setUsername(value);
-          }}
-          />
-        </div>
-        <div className="flex">
-          <label htmlFor="pwd">Password</label>
-          <input 
-            required
-            type="password" 
-            id="pwd" 
-            name="pwd"
-            placeholder="Enter Password"
-            onChange={event => {
-              const { value } = event.target;
-              setPassword(value);
-          }}
-            />
-        </div>
-        <div className="flex">
-          <label htmlFor="media">Upload Profile Picture</label>
-          <input 
-            required
-            type="file" 
-            id="file"
-            accept=".jpg"
-            placeholder="Upload jpg file"
-            onChange={event => {
-              const media = event.target.files[0];
-              setMedia(media);
-          }}
-          />
-        </div>
-      </form>
-      <button onClick={send}>Register</button>
     </div>
   );
 }
