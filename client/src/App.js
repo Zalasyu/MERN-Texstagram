bcbc202cc57d049b893f32ad7c92299bae51edb6
Alec Moldovan <alec_moldovan@yahoo.com>
2022-03-17 20:41:54 -0500

    Changed paths in client/src/App.js for Feed and login page it is now /feed and / respectively.

--- client/src/App.js
+++ client/src/App.js
@@ -6,28 +6,23 @@ import Home from './components/screens/Home.js'
 import Profile from './components/screens/Profile.js'
 import SignUp from './components/screens/SignUp.js'
 import Login from './components/screens/Login.js'
-<<<<<<< HEAD
 import createPost from './components/screens/createPost.js';
-=======
 import Create from './components/screens/Create.js'
 import UpdateBio from './components/screens/UpdateBio.js';
 import { Layout } from 'antd';
->>>>>>> ab7c2d33271511be056cf2a28b039fc7e9fca4f4
 
 // TODO: Implement Profile Pic Upload feature
 function App() {
     return (
       <BrowserRouter>
         <NavBar />
-<<<<<<< HEAD
         <Routes>
-          <Route exact path = "/" component={<Home />} />
+          <Route exact path = "/feed" component={<Home />} />
           <Route path = "/signup" element={<SignUp />} />
-          <Route path = "/login" element={<Login />} />
+          <Route path = "/" element={<Login />} />
           <Route path = "/:username" element={<Profile />} />
           <Route path = "/create" element={<createPost />} />
         </Routes>
-=======
         <Layout>
           <Routes>
             <Route path = "/home" element={<Home />} />
@@ -38,7 +33,6 @@ function App() {
             <Route path = "/update" element={<UpdateBio />} />
           </Routes>
         </Layout>
->>>>>>> ab7c2d33271511be056cf2a28b039fc7e9fca4f4
       </BrowserRouter>
     );
 }
